d2a52d4a8dd6640fb7f19b50ba81ea490872a92c
Jaron <jayisaac6706@gmail.com>
2026-05-20 21:17:29 -0700

    Schema structure started

--- bfd-app/src/scripts/schema.sql
+++ bfd-app/src/scripts/schema.sql
@@ -1,5 +1,10 @@
 DROP TABLE IF EXISTS products;
 
 CREATE TABLE products (
-    
+    id PRIMARY_KEY,
+    productName VARCHAR(255),
+    productDesc VARCHAR(255),
+    productType VARCHAR(50),
+    producePrice DECIMAL(10, 2),
+    image_url VARCHAR(255)
 );
