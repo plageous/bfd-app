6925bc4697509c612d8af6a876a02f41c8e6ccbf
Jaron <jayisaac6706@gmail.com>
2026-06-11 02:17:36 -0700

    Altered SQL query

--- src/services/service.js
+++ src/services/service.js
@@ -1,4 +1,39 @@
 import pool from "../scripts/db.js";
+import bcrypt from "bcrypt";
+
+export const hashPassword = async (plainPassword) => {
+    const saltRounds = 10;
+    return await bcrypt.hash(plainPassword, saltRounds);
+}
+
+
+// password validation
+export const validatePassword = async (plainPassword, storedHash) => {
+    return await bcrypt.compare(plainPassword, storedHash);
+};
+
+//create user
+export const createUser = async (username, plainPassword) => {
+    if(!username) throw new Error("Username is required.");
+    if(!plainPassword) throw new Error("Password is required.");
+
+    //hash password before insert
+    const passwordHash = await hashPassword(plainPassword);
+
+    const [result] = await pool.execute(
+        "INSERT INTO users (username, password) values (?, ?)",
+        [username, passwordHash]
+    );
+}
+
+    
+export const findUserByUsername = async (username) => {
+    const [results] = await pool.query(
+        "SELECT userId, username, password FROM users WHERE username = ? LIMIT 1",
+        [username]
+    );
+    return results[0];
+};
 
 const fetchProducts = async (name = "") => {
     let sql = "SELECT * FROM products";
