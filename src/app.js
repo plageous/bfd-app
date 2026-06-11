9bdf928a12e39c926c82716cf6acacfa7330ce6c
Jaron <jayisaac6706@gmail.com>
2026-06-11 02:11:03 -0700

    Added middleware logic:

--- src/app.js
+++ src/app.js
@@ -5,6 +5,23 @@ import session from "express-session";
 //configure Express.js app
 const app = express();
 
+// session middleware
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+}));
+
+// attaching users to every req
+app.use((req, res, next) => {
+    if (req.session.user) {
+        req.user = req.session.user;
+    }
+    else{
+        req.user = null;
+    }
+    next();
+});
 //view engine
 app.set("view engine", "ejs");
 app.set("views", "src/views");
